d2fdf97ccfd9537e10e4a11b0bbc35cd7152545c
luhaojian1 <962671114@qq.com>
2019-07-16 16:59:56 +0800

    [Allard] create service level

--- src/main/java/com/tw/apistackbase/controller/EmployeesController.java
+++ src/main/java/com/tw/apistackbase/controller/EmployeesController.java
@@ -1,44 +1,47 @@
 package com.tw.apistackbase.controller;
 
 import com.tw.apistackbase.modle.Employee;
-import com.tw.apistackbase.repository.EmployeesRepostory;
+import com.tw.apistackbase.service.EmployeeSerive;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class EmployeesController {
-    public EmployeesRepostory employeesRepostory = new EmployeesRepostory();
+    @Autowired
+    public EmployeeSerive employeeSerive;
 
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee) {
-        return employeesRepostory.createEmployee(employee);
+        return employeeSerive.createEmployee(employee);
     }
 
     @GetMapping("/employees/{employeeId}")
     public Employee findById(@PathVariable String employeeId) {
         System.out.println(employeeId);
-        return employeesRepostory.findById(employeeId);
+        return employeeSerive.findById(employeeId);
     }
 
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return employeesRepostory.findAll();
+        return employeeSerive.findAll();
     }
 
     @DeleteMapping("/employees/{employeeId}")
     public Employee deleteById(@PathVariable String employeeId) {
-        return employeesRepostory.deleteEmployee(employeeId);
+        return employeeSerive.deleteEmployee(employeeId);
     }
 
-    @PutMapping("/employees")
-    public Employee updateEmployee(@RequestBody Employee employee) {
-        return employeesRepostory.updateEmployee(employee);
+    @PutMapping("/employees/{employeeId}")
+    public Employee updateEmployee(@PathVariable String employeeId, @RequestBody Employee employee) {
+        employee.setId(employeeId);
+        return employeeSerive.updateEmployee(employee);
     }
 
     @GetMapping(value = "/employees", params = "ageMini")
     public List<Employee> findAgeAbove(@RequestParam int ageMini) {
         System.out.println(ageMini);
-        return employeesRepostory.findSuitableEmployeesByAge(ageMini);
+        return employeeSerive.findSuitableEmployeesByAge(ageMini);
     }
 }
--- src/main/java/com/tw/apistackbase/repository/EmployeesRepostory.java
+++ src/main/java/com/tw/apistackbase/repository/EmployeesRepostory.java
@@ -2,57 +2,18 @@ package com.tw.apistackbase.repository;
 
 import com.tw.apistackbase.modle.Employee;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.List;
 
-public class EmployeesRepostory {
-    public static Map<String, Employee> employees = new HashMap<>();
+public interface EmployeesRepostory {
+    Employee createEmployee(Employee employee);
 
-    public Employee createEmployee(Employee employee) {
-        String employeeId = UUID.randomUUID().toString();
-        employee.setId(employeeId);
-        employee.setName(employee.getName());
-        employee.setAge(employee.getAge());
-        employee.setGender(employee.getGender());
-        employees.put(employeeId, employee);
-        return employees.get(employeeId);
-    }
+    Employee findById(String employeeId);
 
-    public EmployeesRepostory() {
-        createEmployee("111", "小明", 18, "female");
-        createEmployee("222", "小明2", 12, "female");
-        createEmployee("333", "小明3", 13, "female");
-        createEmployee("444", "小明4", 14, "female");
-    }
+    List<Employee> findAll();
 
-    public void createEmployee(String id, String name, int age, String gender) {
-        Employee employee = new Employee();
-        employee.setId(id);
-        employee.setName(name);
-        employee.setAge(age);
-        employee.setGender(gender);
-        employees.put(id, employee);
-    }
+    Employee updateEmployee(Employee employee);
 
-    public Employee findById(String employeeId) {
-        return employees.get(employeeId);
-    }
+    Employee deleteEmployee(String employeeId);
 
-    public List<Employee> findAll() {
-        return new ArrayList<>(employees.values());
-    }
-
-    public Employee updateEmployee(Employee employee) {
-        employees.put(employee.getId(), employee);
-        return employees.get(employee.getId());
-    }
-
-    public Employee deleteEmployee(String employeeId) {
-        return employees.remove(employeeId);
-    }
-
-
-    public List<Employee> findSuitableEmployeesByAge(int age) {
-        return employees.values().stream().filter(employee -> employee.getAge() > age).collect(Collectors.toList());
-    }
+    List<Employee> findSuitableEmployeesByAge(int age);
 }
